8937dbba637a8ad1a0da21915c761e76c80d2c27
Samet Bilgi <samet.bilgi@yahoo.com>
2023-05-19 15:54:44 +0300

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Samet Bilgi on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
